08c8e1792cee115bd685f6c793fbd68da5712855
gab9244 <gab9244@gmail.com>
2022-12-22 20:25:16 -0300

    Acabei a parte do style e vou comeãr o javascript

---
+++ calculator.js
@@ -0,0 +1,24 @@
+const screen = document.getElementById('screen')
+const clearButton = document.getElementById('clear')
+const deletebutton = document.getElementById('delete')
+const sevenButton = document.getElementById('seven')
+const eightButton = document.getElementById('eight')
+const nineButton = document.getElementById('nine')
+const divideButton = document.getElementById('divide')
+const fourButton = document.getElementById('four')
+const fiveButton = document.getElementById('five')
+const sixButton = document.getElementById('six')
+const timesButton = document.getElementById('times')
+const oneButton = document.getElementById('one')
+const twoButton = document.getElementById('two')
+const threeButton = document.getElementById('three')
+const minusButton = document.getElementById('minus')
+const dotButton = document.getElementById('dot')
+const zeroButton = document.getElementById('zero')
+const equalButton = document.getElementById('equal')
+const sumButton = document.getElementById('sum')
+
+
+
+
+
